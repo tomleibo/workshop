2cc2095a1b715a2e01a3b867b250e72e25689b36
yuvalapidot <yuvalapidot@gmail.com>
2015-04-21 07:58:54 +0300

    Little changes in user controller.

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,10 +1,18 @@
 package controllers;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import content.Forum;
 import users.User;
 
 public class UserController {
-	public User login(String user, String pass) {
-		return new User();
+	public User login(Forum forum, String username, String password) {
+		User user = getUserFromForum(username, forum);
+		if (user == null)
+			return null;
+		
 	}
 	
 	public User enterAsGuest() {
@@ -41,5 +49,24 @@ public class UserController {
 	
 	public boolean deactivate(User member) {
 		return false;
+	}
+	
+	private User getUserFromForum(String username, Forum forum) {
+		List<User> members = forum.getMembers();
+		for (User member : members) {
+			if (member.getUserName().equals(username))
+				return member;
+		}
+		return null;
+	}
+	
+	private String hashString(String string) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			md.update(string.getBytes());
+			return md.digest().toString();
+		} catch (NoSuchAlgorithmException e) {
+			return string;
+		}
 	}
 }
