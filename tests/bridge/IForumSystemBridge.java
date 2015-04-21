fcb6857a760207a77d63a17ba1b6e7d7ecbf03bb
Roee <roeeshachar@gmail.com>
2015-04-22 02:30:21 +0300

    Implemented Bridge Design Factory. Waiting to integrate with implementation

--- tests/bridge/IForumSystemBridge.java
+++ tests/bridge/IForumSystemBridge.java
@@ -2,8 +2,10 @@ package bridge;
 
 import java.util.List;
 
+import exceptions.UserNotAuthorized;
 import policy.ForumPolicy;
 import policy.Policy;
+import users.Report;
 import users.User;
 import content.Forum;
 import content.Message;
@@ -12,22 +14,18 @@ import content.Thread;
 import users.userState.UserStates;
 
 public interface IForumSystemBridge {
+	// General Services
 	List<SubForum> showSubForumList(Forum forum);
 	List<content.Thread> showThreadsList(SubForum subForum);
 	List<content.Thread> getSubForumThreads(SubForum subForum);
 	List<Message> searchMessages(Forum forum, String title, String content, String memberName, java.sql.Date startDate, java.sql.Date endDate);
-	Forum addForum(String title);
+	Forum addForum(String title, User superAdmin, Policy policy);
 	Message replyToMessage(Message addTo, String title, String content, User user);
-	// 
+	// Member Services
 	User registerGuest(Forum forum, String user, String hashedPass);
-	
-	// 
 	User loginUser(Forum forum, String user, String pass);
 	boolean logoffUser(Forum forum, User user);
-	// 
 	Thread openThread(SubForum sbfrm, String title, String content, User user);
-	
-	boolean changeUserState(User user, UserStates state);
 	boolean editPost(Forum forum, User user, Message msg, String body);
 	boolean deletePost(Forum forum, User user, Message msg);
 	boolean reportAdmin(User user, User admin);
@@ -52,8 +50,9 @@ public interface IForumSystemBridge {
 	boolean changeForumPolicy(Forum forum, ForumPolicy policy);
 	boolean sendFriendRequest(User from, User to, String message);
 	boolean removeFriend(User user, User friend);
-
-
-
+	boolean defineProperties(Forum forum, ForumPolicy policy);
+	User enterAsGuest(Forum forum);
+	boolean reportModeratorInForum(Forum forum, User reporter, User admin, String title, String content);
+	boolean deleteSubForum(Forum forum, SubForum subForum,User user);
 
 	}
