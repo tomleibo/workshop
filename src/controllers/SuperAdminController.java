6bff8668c4943902956d639c466932353131b9f3
yuvalapidot <yuvalapidot@gmail.com>
2015-04-25 16:49:27 +0300

    Changed super admin controller method to static

--- src/controllers/SuperAdminController.java
+++ src/controllers/SuperAdminController.java
@@ -14,7 +14,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class SuperAdminController {
 
-	public Forum createNewForum(User superAdmin, ForumPolicy policy, String name) throws UserNotAuthorizedException {
+	public static Forum createNewForum(User superAdmin, ForumPolicy policy, String name) throws UserNotAuthorizedException {
 		if (PolicyHandler.canUserAddForum(superAdmin)) {
 			Forum forum = new Forum(superAdmin, policy, name);
 			ForumSystem.getInstance().addForum(forum);
@@ -23,33 +23,33 @@ public class SuperAdminController {
 		throw new UserNotAuthorizedException("to create new forum.");
 	}
 	
-	public boolean changeAdministrator(User superAdmin, Forum forum, User admin) throws UserNotAuthorizedException {
+	public static boolean changeAdministrator(User superAdmin, Forum forum, User admin) throws UserNotAuthorizedException {
 		if (PolicyHandler.canReplaceAdmin(superAdmin, forum, admin))
 			forum.setAdmin(superAdmin);
 		throw new UserNotAuthorizedException("to change administrator.");
 	}
 	
-	public boolean verifyCorrelationOfContent(User superAdmin, Forum forum, Message msg) {
+	public static boolean verifyCorrelationOfContent(User superAdmin, Forum forum, Message msg) {
 		// TODO
 		return false;
 	}
 
-	public boolean changeForumPolicy(User superAdmin, Forum forum, ForumPolicy policy) throws UserNotAuthorizedException {
+	public static boolean changeForumPolicy(User superAdmin, Forum forum, ForumPolicy policy) throws UserNotAuthorizedException {
 		if (PolicyHandler.canUserChangePolicy(superAdmin, forum))
 			return forum.setPolicy(policy);
 		throw new UserNotAuthorizedException("to change forum policy.");
 	}
 
-	public ForumSystem initializeForumSystem(String username, String password, String email) throws NoSuchAlgorithmException {
+	public static ForumSystem initializeForumSystem(String username, String password, String email) throws NoSuchAlgorithmException {
 		User superAdmin = User.newSuperAdmin(username, Cipher.hashString(password, Cipher.SHA), email);
 		return ForumSystem.newForumSystem(superAdmin);
 	}
 
-	public boolean addUserStatusType(String type, UserStatusPolicy userStatusPolicy){
+	public static boolean addUserStatusType(String type, UserStatusPolicy userStatusPolicy){
 		return ForumSystem.getInstance().addUserStatusType(type, userStatusPolicy);
 	}
 
-	public boolean removeUserStatusType(String type){
+	public static boolean removeUserStatusType(String type){
 		return ForumSystem.getInstance().removeUserStatusType(type);
 	}
 
