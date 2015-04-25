2c027b07083e5b0150f421ff09a4f266827d5158
tomleibo <tomleibo@gmail.com>
2015-04-25 16:30:13 +0300

    Update IdGenerator.java
    Replaced static block with constructor

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -3,13 +3,14 @@ package utils;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class IdGenerator {
-	private static AtomicInteger[] ids;
-	{
+	private AtomicInteger[] ids;
+	private IdGenerator() {
 		ids= new AtomicInteger[NUMBER_OF_TYPES];
 		for (int i=0; i<NUMBER_OF_TYPES;i++) {
 			ids[i] = new AtomicInteger(0);
 		}
-	}
+	}	
+	private static IdGenerator instance=null;
 	private static final int NUMBER_OF_TYPES = 5;
 	public static final int FORUM = 0;
 	public static final int SUBFORUM = 1;
@@ -18,7 +19,14 @@ public class IdGenerator {
 	public static final int USER = 4;
 	
 	public static int getId(int type) {
-		return ids[type].getAndIncrement();
+		if(instance==null) {
+			synchronized (IdGenerator.class) {
+				if(instance==null) {
+					instance = new IdGenerator();
+				}
+			}
+		}
+		return instance.ids[type].getAndIncrement();
 	}
 	
 }
