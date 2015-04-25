6957352b89050ee24f92a75389b995e95bcfa06f
Roee <roeeshachar@gmail.com>
2015-04-25 20:02:51 +0300

    Merge branch 'master' of C:\Users\Roee\IdeaProjects\workshop with conflicts.

--- src/controllers/SuperAdminController.java
+++ src/controllers/SuperAdminController.java
@@ -6,7 +6,6 @@ import content.Message;
 import exceptions.UserNotAuthorizedException;
 import policy.ForumPolicy;
 import policy.PolicyHandler;
-import policy.UserStatusPolicy;
 import users.User;
 import utils.Cipher;
 
@@ -14,8 +13,8 @@ import java.security.NoSuchAlgorithmException;
 
 public class SuperAdminController {
 
-	public Forum createNewForum(User superAdmin, ForumPolicy policy, String name) throws UserNotAuthorizedException {
-		if (PolicyHandler.canUserAddForum(superAdmin)) {
+	public static Forum createNewForum(User superAdmin, ForumPolicy policy, String name) throws UserNotAuthorizedException {
+		if (PolicyHandler.canUserAddSubForum(superAdmin)) {
 			Forum forum = new Forum(superAdmin, policy, name);
 			ForumSystem.getInstance().addForum(forum);
 			return forum;
@@ -23,34 +22,26 @@ public class SuperAdminController {
 		throw new UserNotAuthorizedException("to create new forum.");
 	}
 	
-	public boolean changeAdministrator(User superAdmin, Forum forum, User admin) throws UserNotAuthorizedException {
+	public static boolean changeAdministrator(User superAdmin, Forum forum, User admin) throws UserNotAuthorizedException {
 		if (PolicyHandler.canReplaceAdmin(superAdmin, forum, admin))
 			forum.setAdmin(superAdmin);
 		throw new UserNotAuthorizedException("to change administrator.");
 	}
 	
-	public boolean verifyCorrelationOfContent(User superAdmin, Forum forum, Message msg) {
+	public static boolean verifyCorrelationOfContent(User superAdmin, Forum forum, Message msg) {
 		// TODO
 		return false;
 	}
 
-	public boolean changeForumPolicy(User superAdmin, Forum forum, ForumPolicy policy) throws UserNotAuthorizedException {
+	public static boolean changeForumPolicy(User superAdmin, Forum forum, ForumPolicy policy) throws UserNotAuthorizedException {
 		if (PolicyHandler.canUserChangePolicy(superAdmin, forum))
 			return forum.setPolicy(policy);
 		throw new UserNotAuthorizedException("to change forum policy.");
 	}
 
-	public ForumSystem initializeForumSystem(String username, String password, String email) throws NoSuchAlgorithmException {
+	public static ForumSystem initializeForumSystem(String username, String password, String email) throws NoSuchAlgorithmException {
 		User superAdmin = User.newSuperAdmin(username, Cipher.hashString(password, Cipher.SHA), email);
 		return ForumSystem.newForumSystem(superAdmin);
-	}
-
-	public boolean addUserStatusType(String type, UserStatusPolicy userStatusPolicy){
-		return ForumSystem.getInstance().addUserStatusType(type, userStatusPolicy);
-	}
-
-	public boolean removeUserStatusType(String type){
-		return ForumSystem.getInstance().removeUserStatusType(type);
 	}
 
 }
