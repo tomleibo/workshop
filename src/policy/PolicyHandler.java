ee8fda688519998814bb3057d964d918bf548f46
yuvalapidot <yuvalapidot@gmail.com>
2015-06-08 18:56:57 +0300

    Fixed a bug

--- src/policy/PolicyHandler.java
+++ src/policy/PolicyHandler.java
@@ -69,7 +69,7 @@ public class PolicyHandler {
 	}
 
 	public static boolean canAppointModerator(Forum forum, SubForum subForum, User admin, User moderator) {
-		if(forum.getSubForums().contains(subForum) && forum.getAdmin().equals(admin) && moderator.isActive())
+		if(forum.getSubForums().contains(subForum) && forum.getAdmin().equals(admin) && moderator.isActive() && forum.getMembers().contains(moderator))
 			return true;
 		return  false;
 	}
